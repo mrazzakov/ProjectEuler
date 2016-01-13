24c6eefe31e0e602bcd9d1704d722790a74cc499
michail razzakov <mrazzakov@gmail.com>
2016-01-13 16:54:03 -0600

    proj3 done

--- proj3.cpp
+++ proj3.cpp
@@ -1,11 +1,24 @@
 //Project Euler
 //Michail Razzakov
 
+//Largest prime factor
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//What is the largest prime factor of the number 600851475143 ?
+
+
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
+long int n=600851475143;
+long int i,j,num;
 
+   for(i=1;i<n;i++)
+      if(n%i == 0){
+         n = n/i;
+         i = 1;
+      }
+   cout << "prime: " << n << endl;
 }
-
